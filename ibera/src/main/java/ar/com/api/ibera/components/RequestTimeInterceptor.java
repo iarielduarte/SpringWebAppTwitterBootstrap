e266926245d8470377fcad3c8dbf1546bdd4762b
ariel.duarte <arielpy@gmail.com>
2017-06-13 14:46:09 -0300

    Mechanism to save logs

--- ibera/src/main/java/ar/com/api/ibera/components/RequestTimeInterceptor.java
+++ ibera/src/main/java/ar/com/api/ibera/components/RequestTimeInterceptor.java
@@ -1,18 +1,30 @@
 package ar.com.api.ibera.components;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import ar.com.api.ibera.repositories.LogRepository;
 
 
 @Component("requestTimeInterceptor")
 public class RequestTimeInterceptor extends HandlerInterceptorAdapter{
 
 	private static final Log LOG = LogFactory.getLog(RequestTimeInterceptor.class);
+	
+	@Autowired
+	@Qualifier("logRepository")
+	private LogRepository logRepository;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -25,7 +37,16 @@ public class RequestTimeInterceptor extends HandlerInterceptorAdapter{
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 		long startTime = (long) request.getAttribute("startTime");
-		LOG.info("--REQUEST URL : '"+ request.getRequestURL().toString() + "' -- TOTAL TIME : '" + (System.currentTimeMillis() - startTime) + "'MS");
+		String url = request.getRequestURL().toString();
+		String username = "";
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if(auth != null && auth.isAuthenticated()){
+			username = auth.getName();
+			ar.com.api.ibera.models.Log log = new ar.com.api.ibera.models.Log(new Date(), auth.getDetails().toString(), username, url);
+			logRepository.save(log);
+		}
+		
+		LOG.info("--REQUEST URL : '"+ url + "' -- TOTAL TIME : '" + (System.currentTimeMillis() - startTime) + "'MS");
 	}
 
 	
